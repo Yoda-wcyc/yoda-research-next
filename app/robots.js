b93cbed09fe9fbc942d7645f67180efda2db604d
Yoda-wcyc <iamtwispin@gmail.com>
2026-07-13 09:08:31 +0800

    Update BASE URL for robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,4 +1,4 @@
-const BASE = "https://yoda-research.vercel.app"; // 部署後若綁自訂網域,改這裡
+const BASE = "https://yoda-research-next.vercel.app"; // 部署後若綁自訂網域,改這裡
 export default function robots() {
   return {
     rules: { userAgent: "*", allow: "/" },
